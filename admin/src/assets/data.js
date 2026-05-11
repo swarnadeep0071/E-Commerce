d8179ceb3f9212cb39b12d97a7f6ab2c20d0d9d4
Swarnadeep  Dey <swarnadeep2016dey@gmail.com>
2026-05-11 13:22:38 +0530

    First commit

---
+++ admin/src/assets/data.js
@@ -0,0 +1,34 @@
+import p1_img from './product_1.png'
+import p2_img from './product_2.png'
+import p3_img from './product_3.png'
+import p4_img from './product_4.png'
+
+let data_product = [
+  {
+    id:1,
+    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image:p1_img,
+    new_price:350.00,
+    old_price:550.50,
+  },
+  {id:2,
+    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image:p2_img,
+    new_price:450.00,
+    old_price:720.50,
+  },
+  {id:3,
+    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image:p3_img,
+    new_price:550.00,
+    old_price:700.50,
+  },
+  {id:4,
+    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image:p4_img,
+    new_price:299.00,
+    old_price:599.00,
+  },
+];
+
+export default data_product;
